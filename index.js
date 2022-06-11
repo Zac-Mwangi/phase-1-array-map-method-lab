e203a8e0f67d58255324551e1db4341a0f5f0bd0
Zac-Mwangi <zackmwangi998@gmail.com>
2022-06-11 16:56:30 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+  // let result =tutorials.map((tutorial) => tutorial.split(" ").map((passdss)=>passdss[0].toUpperCase() + passdss.slice(1)).join(" "))
+
+
+  // first map splits the elements in the array where there is a single space - 
+  // Split - change a string into an array of substring and returns the array
+
+  // second map takes the charAt(0) and puts turns it into UpperCase the concatinates with the remaining string finally joins the substrings into one array
+
+  return tutorials.map((tutorial) => tutorial.split(" ").map((passedString)=>passedString.charAt(0).toUpperCase() + passedString.slice(1)).join(" "))
 }
